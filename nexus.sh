d2849d50e0296d488fb8966533404afbba0b89da
Vitor Britto <code@vitorbritto.com.br>
2014-06-03 09:32:32 -0300

    Add Linux Support

--- nexus.sh
+++ nexus.sh
@@ -13,58 +13,35 @@
 #       $ chmod u+x nexus.sh
 #
 # Usage:
-#       ./nexus.sh [options] <port>
+#       ./nexus.sh [options] [commands] <port>
 #
 # Options:
-#       --php           Start a webserver with PHP
-#       --py            Start a webserver with Python
+#       -l, --linux     Using Linux
+#       -m, --mac       Using Mac
+#       -h, --help      Output this help text
+#
+# Commands:
+#       php             Start a webserver with PHP
+#       py              Start a webserver with Python
 #
 # Example:
 #       Start a PHP webserver
-#       $ ./nexus.sh --php 8080
+#       $ ./nexus.sh -m php 8080
 #
 # Alias:
-#       alias nexus="bash ~/path/to/script/nexus.sh"
+#       alias nexuspy="bash ~/path/to/script/nexus.sh -m py 8080"
+#       alias nexusphp="bash ~/path/to/script/nexus.sh -m php 8080"
 #
 # ------------------------------------------------------------------------------
 
-# ------------------------------------------------------------------------------
-# | OS SUPPORTED: MAC AND LINUX                                                |
-# ------------------------------------------------------------------------------
+OPEN_MAC="/usr/bin/open"
+OPEN_GNU="x-www-browser"
 
-if [ "$(uname)" == "Darwin" ]; then
-	browser=open
-	ip=$(ipconfig getifaddr en1)
-elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-	browser=x-www-browser
-	ip=$(LANG=C /sbin/ifconfig  | sed -ne $'/127.0.0.1/ ! { s/^[ \t]*inet[ \t]\\{1,99\\}\\(addr:\\)\\{0,1\\}\\([0-9.]*\\)[ \t\/].*$/\\2/p; }')
-fi
+
 
 # ------------------------------------------------------------------------------
 # | FUNCTIONS                                                                  |
 # ------------------------------------------------------------------------------
-
-# Start webserver Function
-nexus_start() {
-	# Start an HTTP server from a directory, optionally specifying the port
-	if [[ "${1}" == "-py" ]]; then
-		local port="${3:-8000}"
-		echo "HINT: Press CTRL+C to stop webserver"
-		sleep 1 && $browser "http://localhost:${port}/" &
-		# Set the default Content-Type to `text/plain` instead of `application/octet-stream`
-		# And serve everything as UTF-8 (although not technically correct, this doesn’t break anything for binary files)
-		python -c $'import SimpleHTTPServer;\nmap = SimpleHTTPServer.SimpleHTTPRequestHandler.extensions_map;\nmap[""] = "text/plain";\nfor key, value in map.items():\n\tmap[key] = value + ";charset=UTF-8";\nSimpleHTTPServer.test();' "$port"
-	fi
-
-	# Start a PHP server from a directory, optionally specifying the port
-	if [[ "${1}" == "-php" ]]; then
-		local port="${3:-4000}"
-		echo "HINT: Press CTRL+C to stop webserver"
-		sleep 1 && $browser "http://${ip}:${port}/" &
-		php -S "${ip}:${port}"
-	fi
-
-}
 
 # Help Function
 nexus_help() {
@@ -76,15 +53,25 @@ NEXUS - Simple and fast method to start a web server
 ------------------------------------------------------------------------------
 
 Usage:
-	./nexus.sh [options] <port>
-
-Example:
-	Start a PHP webserver
-	$ ./nexus.sh --php 8080
+      ./nexus.sh [options] [commands] <port>
 
 Options:
-	--php           Start a webserver with PHP
-	--py            Start a webserver with Python
+      -l, --linux     Using Linux
+      -m, --mac       Using Mac
+      -h, --help      Output this help text
+
+Commands:
+      php             Start a webserver with PHP
+      py              Start a webserver with Python
+
+Example:
+      Start a PHP webserver
+      $ ./nexus.sh -m php 8080
+
+Alias:
+      alias nexuspy="bash ~/path/to/script/nexus.sh -m py 8080"
+      alias nexusphp="bash ~/path/to/script/nexus.sh -m php 8080"
+
 
 Documentation can be found at https://github.com/vitorbritto/nexus/
 
@@ -97,6 +84,72 @@ EOT
 
 }
 
+# Start a Web Server on Mac
+nexus_mac() {
+    # Start an HTTP server from a directory, optionally specifying the port
+    if [[ "${2}" == "py" ]]; then
+        local PORT="${4:-8000}"
+        echo "HINT: Press CTRL+C to stop webserver"
+        sleep 1 && $OPEN_MAC "http://localhost:${PORT}/" &
+        # Set the default Content-Type to `text/plain` instead of `application/octet-stream`
+        # And serve everything as UTF-8 (although not technically correct, this doesn’t break anything for binary files)
+        python -c $'import SimpleHTTPServer;\nmap = SimpleHTTPServer.SimpleHTTPRequestHandler.extensions_map;\nmap[""] = "text/plain";\nfor key, value in map.items():\n\tmap[key] = value + ";charset=UTF-8";\nSimpleHTTPServer.test();' "$PORT"
+    fi
+
+    if [[ "${2}" == "php" ]]; then
+        local PORT="${4:-4000}"
+        local IP=$(ipconfig getifaddr en1)
+        echo "HINT: Press CTRL+C to stop webserver"
+        sleep 1 && $OPEN_MAC "http://${IP}:${PORT}/" &
+        php -S "${IP}:${PORT}"
+    fi
+    exit
+}
+
+# Start a Web Server on GNU/Linux
+nexus_linux() {
+    # Start an HTTP server from a directory, optionally specifying the port
+    if [[ "${2}" == "py" ]]; then
+        local PORT="${4:-8000}"
+        echo "HINT: Press CTRL+C to stop webserver"
+        sleep 1 && $OPEN_GNU "http://localhost:${PORT}/" &
+        # Set the default Content-Type to `text/plain` instead of `application/octet-stream`
+        # And serve everything as UTF-8 (although not technically correct, this doesn’t break anything for binary files)
+        python -c $'import SimpleHTTPServer;\nmap = SimpleHTTPServer.SimpleHTTPRequestHandler.extensions_map;\nmap[""] = "text/plain";\nfor key, value in map.items():\n\tmap[key] = value + ";charset=UTF-8";\nSimpleHTTPServer.test();' "$PORT"
+    fi
+
+    if [[ "${2}" == "php" ]]; then
+        local PORT="${4:-4000}"
+        local IP=$(LANG=C /sbin/ifconfig  | sed -ne $'/127.0.0.1/ ! { s/^[ \t]*inet[ \t]\\{1,99\\}\\(addr:\\)\\{0,1\\}\\([0-9.]*\\)[ \t\/].*$/\\2/p; }')
+        echo "HINT: Press CTRL+C to stop webserver"
+        sleep 1 && $OPEN_GNU "http://${IP}:${PORT}/" &
+        php -S "${IP}:${PORT}"
+    fi
+    exit
+}
+
+# Main Function
+nexus_main() {
+
+    # Show help option
+    if [[ "${1}" == "-h" || "${1}" == "--help" ]]; then
+        nexus_help ${1}
+        exit
+    fi
+
+    if [[ "${1}" == "-l" || "${1}" == "--linux" ]]; then
+        nexus_linux $*
+        exit
+    fi
+
+    if [[ "${1}" == "-m" || "${1}" == "--mac" ]]; then
+        nexus_mac $*
+        exit
+    fi
+
+}
+
+
 
 # Initialize Nexus
-nexus_start $*
+nexus_main $*
